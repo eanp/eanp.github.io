33eeab635627da107fc14ed68f3b7957731bdf35
Ean Praha <57554775+eanp@users.noreply.github.com>
2025-08-18 12:58:38 +0700

    setup terraform override

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,15 +12,17 @@ resource "cloudflare_record" "quran_subdomain" {
   zone_id = local.zone_id
   name    = "quran"
   type    = "CNAME"
-  value   = var.cname_target   # eanp.github.io
   ttl     = 300
   proxied = var.cloudflare_proxied
+  allow_overwrite = true            
+  content         = var.cname_target 
 }
 
 resource "github_repository_file" "cname_file" {
   repository          = var.repo_name
   file                = "CNAME"
   branch              = var.pages_branch
+  content             = var.custom_domain
   commit_message      = "Set custom domain for GitHub Pages"
-  content             = "${var.custom_domain}\n"
+  overwrite_on_create = true         # <-- izinkan timpa kalau file sudah ada
 }
