30f05999197a1883267519a15bc88c95d0b7e399
eanp <awifamz@gmail.com>
2025-09-08 16:26:41 +0700

    setup project

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: "https://eanp.github.io",
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "static"
+});
---
+++ src/consts.ts
@@ -0,0 +1,30 @@
+import type { Site, Metadata, Socials } from "@types";
+
+export const SITE: Site = {
+  NAME: "ean",
+  EMAIL: "eanpraha@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_WORKS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 5,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "eanp blog",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of my articles.",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "github",
+    HREF: "https://github.com/eanp"
+  },
+  {
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/eanp",
+  }
+];
